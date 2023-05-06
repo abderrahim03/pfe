b368047b3ea13f55d2f86ccb606a0ddce6a1d8d0
AbdellahChahdi <Abdellahchahdi2003@gmail.com>
2023-05-06 19:17:11 +0100

    Design nav

--- front-end/src/LandingPage.js
+++ front-end/src/LandingPage.js
@@ -5,33 +5,11 @@ import ChercherParking from './component/ChercherParking';
 import ConsulterDepenses from './component/ConsulterDepenses';
 import ServiceNombre from './component/ServiceNombre';
 import ShowPage from './component/ShowPage';
-
+import Example from './Navbar';
 const LandingPage = () => {
     const id = useParams()
     return (
         <>
-            <div>
-                <ul >
-                    <div >
-                        <span >
-                            <Link to={'/'} >Home</Link>
-                        </span>
-                    </div>
-                    <li >
-                        <Link to={'/ajoutStationnements'} >ajout Stationnements</Link>
-                    </li>
-                    <li >
-                        <Link to={'/chercherParking'} >chercher Parking</Link>
-                    </li>
-                    <li >
-                        <Link to={'/consulterDepenses'} >consulter Depenses</Link>
-                    </li>
-                    <li >
-                        <Link to={'/serviceNombre'} >service</Link>
-                    </li>
-                </ul>
-            </div><br /> <br /><br /><br />
-
             <div >
                 <Routes>
                     <Route path='/ajoutStationnements' element={<AjoutStationnements />}></Route>
