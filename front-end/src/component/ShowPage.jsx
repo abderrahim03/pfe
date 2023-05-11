d9a7fc0fe0b0b3ec78c909db08e8272b044a06b1
abderrahim03 <agoummadabderrahim@gmail.com>
2023-05-11 18:23:15 +0100

    Modify

--- front-end/src/component/ShowPage.jsx
+++ front-end/src/component/ShowPage.jsx
@@ -41,20 +41,20 @@ const ShowPage = () => {
       {
         parkings.map(parking => {
             if (parking.id == id) {
+
                 return ( 
                   <div key={parking.id} className="ml-32 mt-20 max-w-lg  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <div className=' bg-emerald-500 rounded-lg text-white text-center py-5 w-full font-mono text-3xl'>{parking.name}</div>
                     <div className='flex'>
-                    <div className='p-8 mt-10 mb-8'>
-                    <div className=' flex pb-2'><span className='text-lg mr-10'>Ville:</span><h5 className="ml-12">{parking.city}</h5></div>
-                    <div className=' flex pb-2'><span className='text-lg mr-9'>Place Libre:</span><p className="font-normal text-gray-700 dark:text-gray-400">{parking.nbrPlaceLibre}</p></div>
-                    <div className=' flex pb-2'><span className='text-lg mr-10'>Tarif:</span><p className="font-normal text-gray-700 dark:text-gray-400 ml-12">{TarifPark.prix}</p></div>
+                      <div className='p-8 mt-10 mb-8'>
+                        <div className=' flex pb-2'><span className='text-lg mr-10'>Ville:</span><h5 className="ml-12">{parking.city}</h5></div>
+                        <div className=' flex pb-2'><span className='text-lg mr-9'>Place Libre:</span><p className="font-normal text-gray-700 dark:text-gray-400">{parking.nbrPlaceLibre}</p></div>
+                        <div className=' flex pb-2'><span className='text-lg mr-10'>Tarif:</span><p className="font-normal text-gray-700 dark:text-gray-400 ml-12">{TarifPark.prix}</p></div>
+                      </div>
+                      <div className='w-full bg-black rounded-lg m-4'></div>
                     </div>
-                    <div className='w-full bg-black rounded-lg m-4'></div>
-                    </div>
-                    
-                    
-                    
+                    <p className="font-normal text-gray-700 dark:text-gray-400">{parking.nbrPlaceLibre}</p>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">{TarifPark.prix}</p>
                   </div>
                 )
             }
